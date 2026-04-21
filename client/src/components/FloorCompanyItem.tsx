619ed9ab710e5e7c42f924c640ec76bfa91a119b
Thomas Monzel <thomas.monzel@apprisify.com>
2026-04-21 10:10:33 +0200

    Restrict logo width

--- client/src/components/FloorCompanyItem.tsx
+++ client/src/components/FloorCompanyItem.tsx
@@ -3,7 +3,7 @@ import type { FloorCompany } from "../types";
 
 export default function FloorCompanyItem({ company, key }: { company: FloorCompany, key: Key | null | undefined }) {
     return <div key={key} style={{ display: "flex", alignItems: "center", fontSize: 20, gap: 10 }}>
-        <img src={company.logo} style={{ maxHeight: 50, filter: "saturate(0)" }} />
+        <img src={company.logo} style={{ maxHeight: 50, maxWidth: 50, filter: "saturate(0)" }} />
         {company.name}
     </div>
 }
