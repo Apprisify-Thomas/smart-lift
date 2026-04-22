f55a3f5b49d1cf7213ee55de0bd9f37ad5b934cb
Thomas Monzel <tom.monzel@gmail.com>
2026-04-22 14:27:37 +0200

    Fix image sizing

--- client/src/components/FloorCompanyItem.tsx
+++ client/src/components/FloorCompanyItem.tsx
@@ -10,8 +10,21 @@ export default function FloorCompanyItem({ company }: { company: FloorCompany })
         gap: 10,
       }}
     >
-      <div style={{ minWidth: 100, display: 'flex', justifyContent: 'center' }}>
-        <img src={company.logo} style={{ maxHeight: 60, maxWidth: 100, filter: 'saturate(0)' }} />
+      <div
+        style={{
+          minWidth: 100,
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <img
+          src={company.logo}
+          style={{
+            maxHeight: 60,
+            maxWidth: 100,
+            filter: 'saturate(0)',
+          }}
+        />
       </div>
       {company.name}
     </div>
