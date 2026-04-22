764463558055ca86a10cb50bad36b0d585959477
Thomas Monzel <tom.monzel@gmail.com>
2026-04-22 10:41:05 +0200

    Add date and time widget

--- client/src/components/Widgets.tsx
+++ client/src/components/Widgets.tsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from 'react';
 
 export default function Widgets() {
   const [weather, setWeather] = useState<any>(null);
-  const [weatherIcon, setWeatherIcon] = useState<string | null>(null);
+  //const [weatherIcon, setWeatherIcon] = useState<string | null>(null);
+  const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     fetch('https://wttr.in/?format=j1&lang=de')
       .then((res) => res.json())
       .then((data) => {
         setWeather(data);
-        setWeatherIcon(data?.current_condition?.[0]?.weatherIconUrl?.[0]?.value ?? null);
+        //setWeatherIcon(data?.current_condition?.[0]?.weatherIconUrl?.[0]?.value ?? null);
       })
       .catch((err) => console.error('Weather fetch error:', err));
+
+    const interval = setInterval(() => setCurrentTime(new Date()), 1000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -30,6 +34,24 @@ export default function Widgets() {
           Load
         </h2>
         <p className="text-3xl font-extralight">Medium</p>
+      </div>
+
+      <div style={{ minWidth: 250 }}>
+        <h2 className="text-2xl font-semibold mb-4 flex items-center gap-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="38px"
+            viewBox="0 -960 960 960"
+            width="38px"
+            fill="currentColor"
+          >
+            <path d="M582-298 440-440v-200h80v167l118 118-56 57ZM440-720v-80h80v80h-80Zm280 280v-80h80v80h-80ZM440-160v-80h80v80h-80ZM160-440v-80h80v80h-80ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
+          Date & Time
+        </h2>
+        <p className="text-3xl font-extralight">
+          {currentTime.toLocaleDateString('en-US')} {currentTime.toLocaleTimeString()}
+        </p>
       </div>
 
       <div>
